0206402fbfb3c0808dbec7b325940f4eb87f509d
DESKTOP-3HT93A9\Thomas Visser <thomascrvisser@gmail.com>
2017-11-16 20:19:07 -0700

    tw

--- public/javascripts/comments.js
+++ public/javascripts/comments.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
       jobj = JSON.stringify(myobj);
       $("#json").text(jobj);
   
-      var url = "comment";
+      var url = "comment1";
 	$.ajax({
 		url:url,
 		type: "POST",
@@ -23,7 +23,7 @@ $(document).ready(function(){
       jobj = JSON.stringify(myobj);
       $("#json2").text(jobj);
   
-      var url = "comment";
+      var url = "comment2";
 	$.ajax({
 		url:url,
 		type: "POST",
